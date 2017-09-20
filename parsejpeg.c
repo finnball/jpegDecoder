f05f3c0048a1bb6d90edcfeff1b35f4c71cfc292
finn <finn.ball@codethink.co.uk>
2017-09-20 18:31:34 +0100

    Changed way data is parsed to a state machine. File pointer now increments.

--- parsejpeg.c
+++ parsejpeg.c
@@ -24,147 +24,219 @@ ZZ = [0,  1,  5,  6,  14, 15, 27, 28,
 
 jpeg_t *jpegParse(const uint8_t *fileContents, const uint32_t fileSize)
 {
-  uintptr_t filePtr = 0;
   uint16_t *id = malloc(sizeof(uint16_t));
+  *id = 0;
+
+  if (!id)
+    return NULL;
+  
+  uintptr_t *fileSizePtr = malloc(sizeof(uint32_t));
+
+  if (!fileSizePtr)
+    return NULL;
+
+  *fileSizePtr = fileSize;
 
   jpeg_t *jpeg = initJpeg();
   
-  filePtr = parseHeader(fileContents, filePtr, fileSize, id);
+  parseHeader(&fileContents, fileSizePtr, id);
   
-  if(*id != JPEG_HEADER_SOI)
+  if (*id != JPEG_HEADER_SOI)
     {
       fprintf(stderr, "SOI not found at start \n");
       return NULL;
     }
 
-  filePtr = parseHeader(fileContents, filePtr, fileSize, id);
-
-  if (*id == JFIF_HEADER)
-    {
-      filePtr = parseData(fileContents, filePtr, fileSize, &(jpeg->jfif) );
-      filePtr = parseHeader(fileContents, filePtr, fileSize, id);
-    }
-  
-    if (*id == JPEG_HEADER_COM)
-    {
-      filePtr = parseData(fileContents, filePtr, fileSize, &(jpeg->com) );
-      filePtr = parseHeader(fileContents, filePtr, fileSize, id);
-    }
-      
-  if (*id == JPEG_HEADER_DQT)
-    {
-      filePtr = parseData(fileContents, filePtr, fileSize, &(jpeg->dqt) );
-      filePtr = parseHeader(fileContents, filePtr, fileSize, id);
-    }
-  
-  if (*id == JPEG_HEADER_SOF0)
-    {
-      filePtr = parseData(fileContents, filePtr, fileSize, &(jpeg->sof0) );
-      filePtr = parseHeader(fileContents, filePtr, fileSize, id);
-    }
-
-  if (*id == JPEG_HEADER_DHT)
-    {
-      filePtr = parseData(fileContents, filePtr, fileSize, &(jpeg->dht) );
-      filePtr = parseHeader(fileContents, filePtr, fileSize, id);
-    }
-  
+  //parseHeader(&fileContents, fileSizePtr, id);
+  //parseData(&fileContents, fileSizePtr, &(jpeg->jfif), id);
   
   free(id);
+  free(fileSizePtr);
   return jpeg;
 }
 
-
-uintptr_t parseHeader(const uint8_t *fileContents, uintptr_t filePtr,
-		       const uint32_t fileSize, uint16_t *id)
+uintptr_t parseHeader(const uint8_t **filePtr, uintptr_t *fileSize, uint16_t *id)
 {
   
-  if (!fileContents || fileSize == 0 || (filePtr > fileSize) )
+  if (!filePtr || !fileSize)
     return 0;
 
-  const uint16_t *header = (const uint16_t *)&fileContents[filePtr];
+  if (*fileSize < 2)
+    {
+      fprintf(stderr, "ERROR: fileSize < 2, cannot read\n");
+      return 0;
+    }
+  
+  const uint16_t *header = (const uint16_t *)(*filePtr);
   
   switch(*header)
     {
     case JPEG_HEADER_SOI:
       printf("INFO: SOI\n");
-      filePtr += 2;
+      *filePtr += 2;
+      *fileSize -= 2;
       break;
       
     case JFIF_HEADER:
       printf("INFO: JFIF\n");
-      filePtr += 2;
+      *filePtr += 2;
+      *fileSize -= 2;
       break;
       
     case JPEG_HEADER_COM:
       printf("INFO: COM\n");
-      filePtr += 2;
+      *filePtr += 2;
+      *fileSize -= 2;
       break;
       
     case JPEG_HEADER_SOS:
       printf("INFO: SOS\n");
-      filePtr += 2;
+      *filePtr += 2;
+      *fileSize -= 2;
       break;
       
     case JPEG_HEADER_SOF0:
       printf("INFO: SOF0\n");
-      filePtr += 2;
+      *filePtr += 2;
+      *fileSize -= 2;
       break;
       
     case JPEG_HEADER_SOF2:
       printf("INFO: SOF2\n");
-      filePtr += 2;
+      *filePtr += 2;
+      *fileSize -= 2;
       break;
       
     case JPEG_HEADER_DHT:
       printf("INFO: DHT\n");
-      filePtr += 2;
+      *filePtr += 2;
+      *fileSize -= 2;
       break;
       
     case JPEG_HEADER_DQT:
       printf("INFO: DQT\n");
-      filePtr += 2;
+      *filePtr += 2;
+      *fileSize -= 2;
       break;
       
     case JPEG_HEADER_DRI:
       printf("INFO: DRI\n");
-      filePtr += 2;
+      *filePtr += 2;
+      *fileSize -= 2;
       break;
             
     case JPEG_HEADER_EOI:
       printf("INFO: EOI\n");
+      *filePtr += 2;
+      *fileSize -= 2;
       break;
-            
+          
     default:
       fprintf(stderr, "ERROR: Header not detected \n");
       break;
       return 0;
     }
 
-  if (filePtr > fileSize)
-    {
-      fprintf(stderr, "File pointer larger than file size\n");
-      return 0;
-    }
   
   if (id)
     *id = *header;
-    
   
-  return filePtr;
+  return *fileSize;
 }
 
-uintptr_t parseData(const uint8_t *fileContents, uintptr_t filePtr,
-		    const uint32_t fileSize, uint8_t **buffer)
+uintptr_t parseData(const uint8_t **filePtr, uintptr_t *fileSize, uint8_t **buffer, uint16_t *id)
 {
-  if ( (!fileContents) || (filePtr > fileSize) )
-    return 0;
-    
-  const uint16_t dataLength = (endian_fix16( (uint16_t *)&fileContents[filePtr] )) - 2;
-  filePtr += 2;
-
   
-  //printf("%d\n",dataLength);
+  if (!filePtr || !fileSize || !id)
+    return 0;
+
+  if (*fileSize < 2)
+    {
+      fprintf(stderr, "ERROR: fileSize < 2, cannot read\n");
+      return 0;
+    }
+  
+  switch(*id)
+    {
+    case JPEG_HEADER_SOI:
+      printf("WARNING: No data in SOI to parse... \n");
+      break;
+      
+    case JFIF_HEADER:
+      parseBuffer(filePtr, fileSize, buffer);
+      break;
+      
+    case JPEG_HEADER_COM:
+      *filePtr += 2;
+      *fileSize -= 2;
+      break;
+      
+    case JPEG_HEADER_SOS:
+      *filePtr += 2;
+      *fileSize -= 2;
+      break;
+      
+    case JPEG_HEADER_SOF0:
+      *filePtr += 2;
+      *fileSize -= 2;
+      break;
+      
+    case JPEG_HEADER_SOF2:
+      *filePtr += 2;
+      *fileSize -= 2;
+      break;
+      
+    case JPEG_HEADER_DHT:
+      *filePtr += 2;
+      *fileSize -= 2;
+      break;
+      
+    case JPEG_HEADER_DQT:
+      *filePtr += 2;
+      *fileSize -= 2;
+      break;
+      
+    case JPEG_HEADER_DRI:
+      *filePtr += 2;
+      *fileSize -= 2;
+      break;
+            
+    case JPEG_HEADER_EOI:
+      *filePtr += 2;
+      *fileSize -= 2;
+      break;
+          
+    default:
+      fprintf(stderr, "ERROR: Header not detected \n");
+      break;
+      return 0;
+    }
+
+  return *fileSize;
+}
+
+
+uintptr_t parseBuffer(const uint8_t **filePtr, const uintptr_t *fileSize, uint8_t **buffer)
+{
+
+  if (!filePtr || !fileSize)
+    return 0;
+  
+  if (*fileSize < 2)
+    {
+      fprintf(stderr, "ERROR: fileSize < 2, cannot read\n");
+      return 0;
+    }
+
+  const uint16_t dataLength = (endian_fix16( (uint16_t *)(*filePtr) )) - 2;
+  *filePtr += 2;
+
+  if (*fileSize < dataLength)
+    {
+      fprintf(stderr, "ERROR: fileSize < %d, cannot read\n", dataLength);
+      return 0;
+    }
+
   uint8_t *bufferNew = realloc( *buffer, (dataLength) * sizeof(uint8_t) );
 
   if (!bufferNew)
@@ -172,13 +244,11 @@ uintptr_t parseData(const uint8_t *fileContents, uintptr_t filePtr,
 
   for (uintptr_t i = 0; i < dataLength; ++i)
     {
-      bufferNew[i] = fileContents[filePtr++];
-
-      //     printf(">>%lu %02x\n",i, fileContents[filePtr]);
+      bufferNew[i] = *(*filePtr++);
     }
 
   *buffer = bufferNew;
   
-  return filePtr;
+  return *fileSize;
 }
 

3a8ed7db2e6c33df7787aa375a80dfec8c1a4bd2
finnball <finn.ball@codethink.co.uk>
2017-09-25 13:50:46 +0100

    Formatted

--- parsejpeg.c
+++ parsejpeg.c
@@ -7,21 +7,6 @@
 #include "definitions.h"
 #include "tools.h"
 
-
-// FORMATS:
-//http://vip.sugovica.hu/Sardi/kepnezo/JPEG%20File%20Layout%20and%20Format.htm
-/*
-ZZ = [0,  1,  5,  6,  14, 15, 27, 28,
-      2,  4,  7,  13, 16, 26, 29, 42,
-      3,  8,  12, 17, 25, 30, 41, 43,
-      9,  11, 18, 24, 31, 40, 44, 53,
-      10, 19, 23, 32, 39, 45, 52, 54,
-      20, 22, 33, 38, 46, 51, 55, 60,
-      21, 34, 37, 47, 50, 56, 59, 61,
-      35, 36, 48, 49, 57, 58, 62, 63]
- */
-
-
 jpeg_t *jpegParse(const uint8_t *fileContents, const uint32_t fileSize)
 {
   uint16_t id ;
@@ -31,7 +16,7 @@ jpeg_t *jpegParse(const uint8_t *fileContents, const uint32_t fileSize)
   
   jpeg_t *jpeg = initJpeg();  
   
-  if(parse(&fileContents, &fileSizePtr, jpeg, &id))
+  if (parse(&fileContents, &fileSizePtr, jpeg, &id))
     {
       freeJpeg(jpeg);
       return NULL;
